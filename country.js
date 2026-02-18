89a2a30c9c81e761bc7b13efd5c050d1ee406f8c
pragyamalasi-hub <pragyamalasi@gmail.com>
2026-02-18 18:52:10 +0530

    Handle

--- country.js
+++ country.js
@@ -43,14 +43,14 @@ fetch(`https://restcountries.com/v3.1/name/${variable}?fullText=true`)
                         <p><strong>Native Name: </strong>${country.name.nativeName? Object.values(country.name.nativeName)[0].common: country.name.common}</p>
                         <p><strong>Population: </strong>${country.population.toLocaleString()}</p> 
                         <p><strong>Region: </strong>${country.region}</p> 
-                        <p><strong>Sub Region: </strong>${country.subregion}</p> 
+                        <p><strong>Sub Region: </strong>${country.subregion ?? 'N/A'}</p> 
                         <p><strong>Capital: </strong>${country.capital ? country.capital.join(', ') : 'N/A'}</p>
                     </div>
 
                     <div class="right">
                         <p><strong>Top Level Domain: </strong>${country.tld}</p>  
                         <p><strong>currency: <strong>${country.currencies? Object.values(country.currencies)[0].name: 'N/A'}</p>
-                        <p><strong>Languages: </strong>${Object.values(country.languages).join(', ')}</p> 
+                        <p><strong>Languages: </strong>${country.languages? Object.values(country.languages).join(', '): 'N/A'}</p> 
                     </div>
 
 
